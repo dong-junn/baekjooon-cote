50bb4182b7b5aeb1712289f252453078dcbd5375
dongjun <djl020326@gmail.com>
2025-01-10 13:29:42 +0900

    [Bronze V] Title: A-B, Time: 192 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i1 = sc.nextInt();
+        int i2 = sc.nextInt();
+        System.out.println(i1 - i2);
+    }
+}
