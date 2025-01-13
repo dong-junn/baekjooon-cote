1342e16e18bd9afed182a1a028d62e77dffcd722
dongjun <djl020326@gmail.com>
2025-01-13 21:41:57 +0900

    [Bronze V] Title: 사칙연산, Time: 184 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i1 = sc.nextInt();
+        int i2 = sc.nextInt();
+        System.out.println(i1 + i2);
+        System.out.println(i1 - i2);
+        System.out.println(i1 * i2);
+        System.out.println(i1 / i2);
+        System.out.println(i1 % i2);
+    }
+}
